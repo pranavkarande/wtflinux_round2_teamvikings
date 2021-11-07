37983cdae2845a94e250a71ff071e64b10ff2b71
Pranav Karande <pranav.3943@gmail.com>
2021-11-07 20:52:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,16 +5,20 @@ then echo "Please run as root"
      exit
 fi
 
+# NOTE: Your machine may have different name instead of BATT
 BATTERY_PERCENTAGE="$(cat /sys/class/power_supply/BATT/capacity)"
 BATTERY_CHARGING="$(cat /sys/class/power_supply/BATT/status)"
 
 if [[ $BATTERY_CHARGING == "Charging" ]]; then
-    echo "Battery is charging setting governor to performance"
-    echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq
-elif [[ $BATTERY_PERCENTAGE == 100 ]]; then
-    echo "Battery is fully charged setting governor to conservative"
-    echo "conservative" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq
+    echo "Connected to AC setting governor to performance"
+    echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 elif [[ $BATTERY_PERCENTAGE < 30 ]]; then
     echo "Battery is low setting governor to powersave"
-    echo "poewersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq
+    echo "powersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+elif [[ $BATTERY_PERCENTAGE < 50 ]]; then
+    echo "Battery is moderately charged setting governor to conservative"
+    echo "conservative" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+else
+    echo "Battery is well charged setting governor to ondemand"
+    echo "ondemand" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 fi
